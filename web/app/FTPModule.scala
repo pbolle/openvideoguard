00adf6e924889346a0348ac72fc98681279562fa
jan-philipp.bolle <jan-philipp.bolle@aperto.de>
2015-07-15 19:43:04 +0200

    in combination of one module and global the initialisation sequence shut be ok

--- web/app/FTPModule.scala
+++ web/app/FTPModule.scala
@@ -1,5 +1,12 @@
+import java.util.{HashMap, Map}
 import javax.inject.{Inject, Provider, Singleton}
 
+import akka.actor.ActorSystem
+import org.apache.ftpserver.ftplet.{Ftplet, UserManager}
+import org.apache.ftpserver.listener.ListenerFactory
+import org.apache.ftpserver.usermanager.{ClearTextPasswordEncryptor, PropertiesUserManagerFactory}
+import org.apache.ftpserver.{FtpServer, FtpServerFactory}
+import org.openguard.core.ImageFtplet
 import play.api.db.DBApi
 import play.api.inject.{Binding, Module}
 import play.api.{Configuration, Environment}
@@ -15,17 +22,57 @@ class FTPModule() extends Module {
   }
 }
 
-@Singleton
-class FTPApplication @Inject()(dbApi: DBApi) {
-  def start(): Unit = {
-    println("test")
-  }
+object FTPApplication {
+  var dbApi: DBApi = null
+  var started = false
 
-  start()
+  def start(): Unit = {
+    println("dbApi" + dbApi.database("default"))
+
+    // init AKKA
+    implicit val actorsystem = ActorSystem("openvideoguard")
+
+    println("Start FTP Server")
+    // init FTP
+    val serverFactory: FtpServerFactory = new FtpServerFactory()
+    val listenerFactory: ListenerFactory = new ListenerFactory()
+
+    listenerFactory.setPort(2221);
+    listenerFactory.setServerAddress("localhost")
+    listenerFactory.setImplicitSsl(false);
+
+    serverFactory.addListener("default", listenerFactory.createListener())
+
+    val userManagerFactory: PropertiesUserManagerFactory = new PropertiesUserManagerFactory()
+
+    userManagerFactory.setUrl(getClass.getResource("/userManager.properties"))
+    userManagerFactory.setPasswordEncryptor(new ClearTextPasswordEncryptor())
+    val userManager: UserManager = userManagerFactory.createUserManager()
+
+    // init ftplet
+    val ftplets: Map[String, Ftplet] = new HashMap[String, Ftplet]
+    ftplets.put("imageFtplet", new ImageFtplet)
+    serverFactory.setFtplets(ftplets);
+
+    serverFactory.setUserManager(userManager)
+
+    // start the server
+    val server: FtpServer = serverFactory.createServer()
+    server.start()
+
+  }
+}
+
+class FTPApplication {
 }
 
 @Singleton
 class FTPApplicationProvider @Inject()(dbApi: DBApi) extends Provider[FTPApplication] {
-
-  lazy val get = new FTPApplication(dbApi)
+  var instannce = None: Option[FTPApplication]
+  val get = {
+    if (FTPApplication.dbApi == null) {
+      FTPApplication.dbApi = dbApi
+    }
+    new FTPApplication
+  }
 }
