42a09e24df14be80a96bb1e24ad916b217bbe973
jan-philipp.bolle <jan-philipp.bolle@aperto.de>
2015-07-19 14:23:37 +0200

    add stopping server

--- web/app/Global.scala
+++ web/app/Global.scala
@@ -6,5 +6,8 @@ object Global extends GlobalSettings {
   override def onStart(app: Application) {
     FTPApplication.start()
   }
+  override def onStop(app: Application) {
+    FTPApplication.stop()
+  }
 
 }
