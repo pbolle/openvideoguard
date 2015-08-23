0b962ce2744b2048771c8cae24175a6a92a57cd7
jan-philipp.bolle <jan-philipp.bolle@aperto.de>
2015-08-23 15:09:11 +0200

    save video to db

--- core/src/main/scala/org/openguard/core/actor/LoadVideo.scala
+++ core/src/main/scala/org/openguard/core/actor/LoadVideo.scala
@@ -1,8 +1,15 @@
 package org.openguard.core.actor
 
 import java.io.File
+import java.sql.Timestamp
+import java.time.format.DateTimeFormatter
+import java.time.{LocalTime, LocalDate}
+import java.util.Date
 
 import akka.actor.Actor
+import org.apache.commons.io.FileUtils
+import org.openguard.core.dao.ImageRefDAO
+import org.openguard.core.models.ImageRef
 import play.api.Play
 import play.api.Play.current
 
@@ -13,8 +20,10 @@ import scala.sys.process._
  * Created by pbolle on 02.08.15.
  */
 class LoadVideo extends Actor {
+  def imageRefDAO = new ImageRefDAO
   var homeDir = Play.configuration.getString("ftp.homedirectory").getOrElse("~/")
   var tempDir = Play.configuration.getString("ovg.tempDir").getOrElse("~/temp")
+  val formatDay = DateTimeFormatter.ofPattern("hh:mm:ss.SSS")
 
   def receive = {
     case videoPath: String => {
@@ -22,12 +31,45 @@ class LoadVideo extends Actor {
       if (!tempImgDir.exists) {
         tempImgDir.mkdirs
       }
+      // convert movie to pictures
       val toImagesCommand = "avconv -i " + videoPath + " -r 1 -ss 00:00:05 -t 00:01:03 -f image2 " + tempImgDir + File.separator + "lva%04d.png"
-      println(toImagesCommand !)
+      // TODO check exitcode
+      val toImagesExitcode = toImagesCommand !
+
+      // create animated gif
       val animatedGifName = "lva" + System.currentTimeMillis() + ".gif"
       val toAnimatedGifCommand = "convert -delay 2 -loop 0 " + tempImgDir + File.separator + "*.png -scale 256x144 " + tempImgDir + File.separator + animatedGifName
-      println(toAnimatedGifCommand !)
+      // TODO check exitcode
+      val toAnimatedGifExitcode =  toAnimatedGifCommand !
 
+      // TODO remove duplicate code in LoadImage
+      val localDate = LocalDate.now
+      var imgDir = new File(homeDir + File.separator + localDate)
+      if (!imgDir.exists) {
+        imgDir.mkdirs
+      }
+      val now = LocalTime.now
+      val timeStamp = formatDay.format(now);
+
+      // copy to targed
+      val videoType = videoPath.substring(videoPath.lastIndexOf("."))
+      val targedGif = imgDir.getAbsolutePath + File.separator + "sm" + timeStamp + ".gif"
+      val targetVideo = imgDir.getAbsolutePath + File.separator + timeStamp +videoType
+
+      FileUtils.copyFile(new File( tempImgDir + File.separator + animatedGifName),new File(targedGif))
+      FileUtils.copyFile(new File( videoPath),new File(targetVideo))
+
+      // insert in db
+      var imageRef = new  ImageRef(
+        localDate + File.separator + timeStamp + videoType,
+        localDate + File.separator +"sm"+ timeStamp + ".gif",
+        new Timestamp((new Date()).getTime),
+        localDate.getYear,
+        localDate.getMonthValue,
+        localDate.getDayOfMonth,
+        now.getHour
+      )
+      imageRefDAO.insert(imageRef)
     }
     case _ => println("received unknown message")
   }
