47acccc98db0b460a5a27605f263285b5e847532
jan-philipp.bolle <jan-philipp.bolle@aperto.de>
2015-12-21 19:43:38 +0100

    add support for reimport videos

--- core/src/main/scala/org/openguard/core/actor/Reimport.scala
+++ core/src/main/scala/org/openguard/core/actor/Reimport.scala
@@ -18,12 +18,13 @@ class Reimport extends Actor {
   override def receive: Receive = {
     case path: String => {
       for (subDir <- (new File(path)).listFiles().toStream.filter(_.isDirectory).iterator) {
-        importSubDir(subDir)
+        importImagesInDir(subDir)
+        importVideosInDir(subDir)
       }
     }
   }
 
-  def importSubDir(subDir: File) = {
+  def importImagesInDir(subDir: File) = {
     for (file <- subDir.listFiles().toStream.filter(_.getName.endsWith(".png")).filter(!_.getName.startsWith("sm"))) {
       val timeStamp = file.getName.substring(0, file.getName.length - 4)
 
@@ -43,6 +44,28 @@ class Reimport extends Actor {
       imageRefDAO.insert(imageRef)
 
     }
+  }
+
+  def importVideosInDir(subDir: File) = {
+    for (file <- subDir.listFiles().toStream.filter(_.getName.endsWith(".webm"))) {
+      val timeStamp = file.getName.substring(0, file.getName.length - 5)
+      val date = formatDay.parse(subDir.getName + " " + timeStamp)
+
+      // insert in db
+      var imageRef = new ImageRef(
+        subDir.getName + File.separator + file.getName,
+        subDir.getName + File.separator  + "sm" + timeStamp + ".gif",
+        new Timestamp(date.getTime),
+        subDir.getName.substring(0, 4).toInt,
+        subDir.getName.substring(5, 7).toInt,
+        subDir.getName.substring(8, 10).toInt,
+        timeStamp.substring(0, 2).toInt,
+        VIDEO
+      )
+      imageRefDAO.insert(imageRef)
+
+    }
+
   }
 
 }
